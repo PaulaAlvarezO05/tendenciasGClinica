ce8aa57b5fac7bee0eb30f6cb3bcd8fb768c9190
PaulaAlvarezO05 <paula.alvarez@correo.tdea.edu.co>
2024-11-01 20:17:01 -0500

    ajuste2

--- backend/Clinica/settings.py
+++ backend/Clinica/settings.py
@@ -163,4 +163,4 @@ STORAGES = {
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = ["http://localhost:5173",
-                        "https://gestion-clinica.vercel.app"]
+                        "https://gestion-clinica-ghpq7z727-paula-alvarezs-projects.vercel.app"]
