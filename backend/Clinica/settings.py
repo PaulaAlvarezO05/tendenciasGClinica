c05239cd605f2ffd8ecf9d556f88bb9aa02ac3e8
PaulaAlvarezO05 <paula.alvarez@correo.tdea.edu.co>
2024-11-01 19:50:41 -0500

    ajuste

--- backend/Clinica/settings.py
+++ backend/Clinica/settings.py
@@ -162,4 +162,5 @@ STORAGES = {
 }
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOWED_ORIGINS = ["http://localhost:5173"]
+CORS_ALLOWED_ORIGINS = ["http://localhost:5173",
+                        "https://gestion-clinica.vercel.app/"]
--- frontend/Clinica/src/api/Clinica.api.js
+++ frontend/Clinica/src/api/Clinica.api.js
@@ -1,7 +1,9 @@
 import axios from 'axios'
+const API_URL = import.meta.env.VITE_API_URL
+
 
 const clinicaApi = axios.create({
-    baseURL: 'http://127.0.0.1:8000/api/'
+    baseURL: `${API_URL}/api/`
 })
 
 // Implementación de JWT
